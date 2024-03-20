a2afb8ddeded1630d123b24cfc9a23fe8c9e80e5
Julian Kingman <julian.kingman@gmail.com>
2024-03-20 18:26:53 -0500

    export init function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import actions from "./actions";
 import runHook from "./runHook";
+import init from "./init";
 import { Triggers, Hook, ActionName, TriggerName } from "./types";
 
-export { actions, runHook, Triggers, Hook, ActionName, TriggerName }
+export { actions, runHook, Triggers, Hook, ActionName, TriggerName, init }
