299c97a5c4b8bf12680ec3f2ae523917758221ef
Julian Kingman <julian.kingman@gmail.com>
2024-03-26 09:46:03 -0500

    start

---
+++ src/actions/createStory.ts
@@ -0,0 +1,9 @@
+export interface CreateStoryParameters {
+  organizationId: string;
+  initiativeId: string;
+  name: string;
+  description: string;
+  amount: number;
+  image: string;
+  tokenId: string;
+}
